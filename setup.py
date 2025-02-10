43972510bd2570541035b8594db462cd19c4a303
jay <jongyun.kim@manchester.ac.uk>
2025-02-09 23:01:07 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gym_pybullet_drones',
-    version='0.6.0',
-    install_requires=['numpy', 'Pillow', 'matplotlib', 'cycler', 'gym', 'pybullet', 'stable_baselines3', 'ray[rllib]']
+    version='1.0.0',
+    packages=find_packages(include=["gym_pybullet_drones", "gym_pybullet_drones.*"]),
+    install_requires=[
+        'numpy==1.23.4',
+        'Pillow==9.3.0',
+        'matplotlib==3.6.2',
+        'cycler==0.11.0',
+        'gym==0.23.1',
+        'pybullet==3.2.5',
+        # 'stable_baselines3==1.6.2',
+        'ray[rllib]==2.1.0'
+    ]
 )
